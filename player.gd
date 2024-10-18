bf8b6a66e91c017ede5c70814207aeabaa989379
niickbermudeez <146813170+niickbermudeez@users.noreply.github.com>
2024-10-18 15:44:17 +0200

    assignacióDeTecles

--- player.gd
+++ player.gd
@@ -5,7 +5,7 @@ var screen_size # Mida de la finestra de joc.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
